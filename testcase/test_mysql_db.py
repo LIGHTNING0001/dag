a2526256ea1bd34aa91c837baa9b6b32162718d5
lishanjie914@gmail.com <lishanjie914@gmail.com>
2023-06-09 18:43:19 +0800

    sql

--- testcase/test_mysql_db.py
+++ testcase/test_mysql_db.py
@@ -4,22 +4,46 @@ from datasource.mysql_db import query, upsert
 datasource = '2023-02-07'
 
 def test_db():
-    result_set = query("select * from barbeyond.t_data_logic_table_col")
+
+
+    # 日频因子
+    daily_factor_sql = "select * from t_data_logic_table_col where data_physical_table_id in (" \
+        "select data_physical_table_id from t_data_physical_table where type = 'BARBEYOND_DAILY' \
+    )"
+
+    result_set = query(daily_factor_sql)
     print(result_set)
+
+    #   稀疏因子
+    sparse_sql = "select * from t_data_logic_table_col where data_physical_table_id in (" \
+        "select data_physical_table_id from t_data_physical_table where type = 'BARBEYOND_SPARSE' \
+    )"
+
+    sparse_factor = query(sparse_sql)
+
+    macro_sql =  "select * from t_data_logic_table_col where data_physical_table_id in (" \
+        "select data_physical_table_id from t_data_physical_table where type = 'BARBEYOND_MACRO' \
+    )"
 
     # 检查mysql表里因子的end时间(col表和result表)
     for row in result_set:
-        pass
+
+
+
+        if row['end'] != datasource:
+            print(row['full_code'] + "未更新到最新时间")
 
     codes = []
     for full_code in codes:
         items = query("select item, `to` from barbeyond.t_schedular_data_update_result where subject={}".format(full_code))
 
         for item in items:
+
             pass
 
     # 对 `to` 进行检验
     # 检验因子值从两个方面验证：1. 判断是否算出来。2. 判断值正确性（不好实现）
+
 
 
     # 检查 barbyond 因子的 meta, 通过查询因子判断（增量更新一天的）
